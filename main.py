45f8db5be93986e0ad2ad1b6c42a2af80b3290bc
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2024-07-22 18:59:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,29 @@ import requests
 from PIL import Image
 from io import BytesIO
 
-def download_and_run_github_file(url):
-    # GitHub raw content URL
-    raw_url = url.replace('github.com', 'raw.githubusercontent.com').replace('/blob/', '/')
+# def download_and_run_github_file(url):
+#     # GitHub raw content URL
+#     raw_url = url.replace('github.com', 'raw.githubusercontent.com').replace('/blob/', '/')
     
-    # Send a GET request to the URL
-    response = requests.get(raw_url)
+#     # Send a GET request to the URL
+#     response = requests.get(raw_url)
     
-    # Check if the request was successful
-    if response.status_code == 200:
-        # Get the content of the file
-        script_content = response.text
+#     # Check if the request was successful
+#     if response.status_code == 200:
+#         # Get the content of the file
+#         script_content = response.text
         
-        # Print the content of the script (optional, for debugging purposes)
-        # print(script_content)
+#         # Print the content of the script (optional, for debugging purposes)
+#         # print(script_content)
         
-        # Execute the script
-        exec(script_content, globals())
-    else:
-        print(f"Failed to retrieve file. Status code: {response.status_code}")
+#         # Execute the script
+#         exec(script_content, globals())
+#     else:
+#         print(f"Failed to retrieve file. Status code: {response.status_code}")
 
-# Example usage
-github_file_url = "https://github.com/RobertsRob/1231waedewa2131/blob/main/main.py"
-download_and_run_github_file(github_file_url)
+# # Example usage
+# github_file_url = "https://github.com/RobertsRob/1231waedewa2131/blob/main/main.py"
+# download_and_run_github_file(github_file_url)
 
 
 def download_and_open_image(url):
